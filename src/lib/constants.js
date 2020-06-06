d367e562f27a7c05214c15b0313aa53e3fc182fd
Joseph Semrai <josephsemrai@gmail.com>
2020-06-06 02:39:28 -0400

    Add constant disclaimer

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,1 +1,3 @@
 export const taskbarHeight = 28;
+
+// Do not hardcode Windows height or width here as these values will only be calculated upon Windows initialization and will not reflect browser window resizing
