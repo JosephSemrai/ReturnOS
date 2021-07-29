ee55e3df8b98dd67104d4f961bbeaba176e0cb40
Joseph Semrai <josephsemrai@gmail.com>
2021-07-28 22:45:45 -0400

    Remove right click removal

--- src/lib/useEventListener.js
+++ src/lib/useEventListener.js
@@ -1,30 +1,27 @@
 import { useEffect, useRef } from 'react';
 
 export function useEventListener(el, event, callback, options) {
-  const savedCallback = useRef();
-  useEffect(() => {
-    savedCallback.current = callback;
-  });
-
-  useEffect(() => {
-    if ('current' in el) {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      el = el.current;
-    }
-  });
-
-  useEffect(() => {
-    function handler(e) {
-      savedCallback.current(e);
-    }
-
-    if (el) {
-      el.addEventListener(event, handler, options);
-      return () => {
-        el.removeEventListener(event, handler, options);
-      };
-    }
-  }, [el, event, options]);
+  // const savedCallback = useRef();
+  // useEffect(() => {
+  //   savedCallback.current = callback;
+  // });
+  // useEffect(() => {
+  //   if ('current' in el) {
+  //     // eslint-disable-next-line react-hooks/exhaustive-deps
+  //     el = el.current;
+  //   }
+  // });
+  // useEffect(() => {
+  //   function handler(e) {
+  //     savedCallback.current(e);
+  //   }
+  //   if (el) {
+  //     el.addEventListener(event, handler, options);
+  //     return () => {
+  //       el.removeEventListener(event, handler, options);
+  //     };
+  //   }
+  // }, [el, event, options]);
 }
 
 export default useEventListener;
